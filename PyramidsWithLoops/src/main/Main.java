88b27322e4a353b1a2606e8304f45818ce755493
nancera <nancera@LCBLAB2110-36.uccob.uc.edu>
2019-09-19 09:16:39 -0400

    WIP

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -12,14 +12,32 @@
 package main;
 
 public class Main {
+	public static void main(String[] args) {
+        int lines = 5, j = 0;
+        for(int i = 1; i <= lines; ++i, j = 0) {
+            for(int space = 1; space <= lines - i; ++space) {
+                System.out.print("  ");
+            }
+            while(j != 2 * i - 1) {
+                System.out.print(i);
+                ++j;
+            }
+            System.out.println();
+        }
+	}
+}
+	
 
+
+/*
 	public static void main(String[] args) {
 		int lines = 5;
-		for (int i = 0; i < lines; i++) {
-			for (int j = 0; j < i+1; j++) {
+		for (int i = 1; i < lines; i++) {
+			for (int j = 1; j < i+1; j++) {
 				System.out.print(j);
 			}
 			System.out.println("");
 		}
 	}
 }
+*/
