1623d8a65607a37e2ca8eb4332827cab719d7d76
nancera <nancera@vdi-LCB-47.ad.uc.edu>
2019-09-26 06:59:10 -0400

    Final Commit for Project

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -1,6 +1,9 @@
 /*
  * Ryan Nance
  * nancera@mail.uc.edu
+ * Sources: https://www.geeksforgeeks.org/pascal-triangle/ 
+ * Due Date: 09/26/2019
+ * Project Description:
  * Printing pyramids using loops. It kind-of works. 
  * What I really want is
         1
@@ -13,31 +16,17 @@ package main;
 
 public class Main {
 	public static void main(String[] args) {
-        int lines = 5, j = 0;
-        for(int i = 1; i <= lines; ++i, j = 0) {
+        int lines = 5, j = 0; //requested number of lines, lines =5
+        for(int i = 1; i <= lines; ++i, j = 0) {//
             for(int space = 1; space <= lines - i; ++space) {
-                System.out.print("  ");
+                System.out.print(" "); //for loop to print spaces to keep code center alligned and spaced 
             }
-            while(j != 2 * i - 1) {
+            while(j != 2 * i - 1) { //prints an incremental pyramid
                 System.out.print(i);
                 ++j;
             }
-            System.out.println();
+            System.out.println();//prints all print statements
         }
 	}
 }
 	
-
-
-/*
-	public static void main(String[] args) {
-		int lines = 5;
-		for (int i = 1; i < lines; i++) {
-			for (int j = 1; j < i+1; j++) {
-				System.out.print(j);
-			}
-			System.out.println("");
-		}
-	}
-}
-*/
